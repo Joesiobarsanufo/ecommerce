ea63864477cfe8e329cafdf6c602e239f4318d95
Joesiobarsanufo <joesiobarsanufo@live.com>
2021-03-11 14:32:56 -0300

    Criando template

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,49 +1,5 @@
 <?php
-/*
-namespace hcode;
 
-use Rain\Tpl;
-
-class Page{
-	private $tpl;
-	private $options=[];
-	private $defaults=[
-       "data"=>[]
-       
-       
-	];
-
-
-	public function __construct($opts=array()){
-		$this->options = array_merge($this->defaults, $opts);
-$config = array(
-	              
-					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-					"debug"         => false // set to false to improve the speed
-				   );
-	}
-	Tpl::configure( $config );
-	$this->$tpl = new Tpl;
-	$this->setData($this->options["data"]);
-	$this->tpl->draw("header");
-}
-private function setData($data=array()){
-	foreach ($this->options["data"] as $key => $value) {
-		$this->tpl->assign($key,$value);
-	}
-}
-public function setTpl($name,$data=array(),$returnHTML=false){
-$this->setData($data);
-return $this->tpl->draw($name,$returnHTML);
-}
-
-public function __destruct(){
-$this->tpl->draw("footer");
-
-}
-}
-*/
 namespace Hcode;
 
 use Rain\Tpl;
@@ -58,14 +14,14 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array())
+	public function __construct($opts = array(), $tpl_dir ="/views/")
 	{
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
 		    "base_url"      => null,
-		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
 		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 		    "debug"         => false
 		);
